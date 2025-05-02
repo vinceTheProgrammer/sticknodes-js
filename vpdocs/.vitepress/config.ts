484b4b658dbcc9cbc84814f25e1c30846df72e25
vincetheprogrammer <vincetheprogrammer@gmail.com>
2025-05-01 22:15:04 -0500

    scaffold guide

--- vpdocs/.vitepress/config.ts
+++ vpdocs/.vitepress/config.ts
@@ -18,7 +18,41 @@ export default defineConfig({
       {
         text: 'Introduction',
         items: [
+          { text: 'About sticknodes-js', link: '/about' },
           { text: 'Getting Started', link: '/getting-started' }
+        ]
+      },
+      {
+        text: 'Stickfigures',
+        items: [
+          { text: 'Anatomy of a Stickfigure', link: '/stickfigures/anatomy' },
+          { text: 'Create', link: '/stickfigures/create' },
+          { text: 'Modify', link: '/stickfigures/modify' },
+          { text: 'Create from bytes', link: '/stickfigures/create-from-bytes' },
+          { text: 'Export to bytes', link: '/stickfigures/export-to-bytes' }
+        ]
+      },
+      {
+        text: 'Nodes',
+        items: [
+          { text: 'Anatomy of a Node', link: '/nodes/anatomy' },
+          { text: 'Get', link: '/nodes/get' },
+          { text: 'Create', link: '/nodes/create' },
+          { text: 'Modify', link: '/nodes/modify' },
+          { text: 'Delete', link: '/nodes/delete' },
+          { text: 'Bulk management', link: '/nodes/bulk' },
+          { text: 'Examples', link: '/nodes/examples' },
+        ]
+      },
+      {
+        text: 'Polyfills',
+        items: [
+          { text: 'Anatomy of a Polyfill', link: '/polyfills/anatomy' },
+          { text: 'Get', link: '/polyfills/get' },
+          { text: 'Create', link: '/polyfills/create' },
+          { text: 'Modify', link: '/polyfills/modify' },
+          { text: 'Delete', link: '/polyfills/delete' },
+          { text: 'Examples', link: '/polyfills/examples' },
         ]
       }
     ],
